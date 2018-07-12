0ac457dac6ee6cb66f922c58a6ed12804002463d
IsaacPilatuna <isapila1996@hotmail.com>
2018-07-12 09:18:29 -0500

    Clase persona

---
+++ ModulosG4/ModulosG4/Persona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModulosG4
+{
+    class Persona
+    {
+        private String numeroDeCedula { get; set; }
+        private String nombres { get; set; }
+        private String apellidos { get; set; }
+
+        private String obtenerNombreCompleto()
+        {
+            return nombres + " " + apellidos;
+        }
+
+
+    }
+}
